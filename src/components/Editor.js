4fb6d21582ff3bf2ebf86e6d70545f3daa757199
Difficult-world24 <kar232442@gmail.com>
2022-04-29 11:21:23 +0530

    State and methods added

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -2,13 +2,31 @@ import React from "react";
 import { Grid } from "@mui/material";
 import { withStyles } from "@mui/styles";
 import EditorInput from "./EditorInput";
-import EditorFooter from "./EditorFooter";
+import EditorConsole from "./EditorConsole";
 import EditorNavBar from "./EditorNavBar";
 import EditorPreviewer from "./EditorPreviewer";
 import Markdown from "./context/MarkdownContext";
 import styles from "./styles/EditorStyles";
 
 class Editor extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { showPreviewer: true, showEditorConsole: true };
+    this.togglePreviewer = this.togglePreviewer.bind(this);
+    this.toggleEditorConsole = this.toggleEditorConsole.bind(this);
+  }
+
+  togglePreviewer() {
+    this.setState((prevState) => {
+      return { showPreviewer: !prevState.showPreviewer };
+    });
+  }
+
+  toggleEditorConsole() {
+    this.setState((prevState) => {
+      return { showEditorConsole: !prevState.showEditorConsole };
+    });
+  }
   render() {
     const { classes } = this.props;
     return (
@@ -22,7 +40,7 @@ class Editor extends React.Component {
             <EditorPreviewer />
           </Grid>
           <Grid item gridRow="3">
-            <EditorFooter />
+            <EditorConsole />
           </Grid>
         </Grid>
       </Markdown>
